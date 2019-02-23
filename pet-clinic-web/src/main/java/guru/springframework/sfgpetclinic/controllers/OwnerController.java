78d1dac41c5f24ffa1e7104453acd208786715e7
DESKTOP-5GLTOPK\faygu <faygun89@gmail.com>
2019-02-23 16:27:19 +0300

    Created Controller and index page for vet and owner. Closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String ListOwners(){
+        return "owners/index";
+    }
+}
